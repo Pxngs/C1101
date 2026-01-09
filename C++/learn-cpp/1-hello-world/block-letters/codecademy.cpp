63649c3428b27dda48625bec4a750f421a3b196d
Pxngs <kaleb_thomas2@icloud.com>
2026-01-09 11:15:25 -0500

    Create codecademy.cpp

---
+++ C++/learn-cpp/1-hello-world/block-letters/codecademy.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+  int main () {
+
+    std::cout << " ____________________               \n";
+    std::cout << "| |________________| |              \n";
+    std::cout << "| |                | |              \n";
+    std::cout << "| |                | |              \n";
+    std::cout << "| |                | |              \n";
+    std::cout << "| |    XXXXX       | |              \n"; 
+    std::cout << "| |  XX     X      | |              \n";
+    std::cout << "| |  X             | |              \n";
+    std::cout << "| |  X             | |              \n";
+    std::cout << "| |  X             | |              \n";
+    std::cout << "| |  XX     X      | |              \n";
+    std::cout << "| |    XXXXX       | |              \n";
+    std::cout << "| |                | |  ==========  \n";
+    std::cout << "| |----------------| | |          | \n";
+    std::cout << "|____________________|  ==========  \n";
+
+    return 0;
+
+  }
