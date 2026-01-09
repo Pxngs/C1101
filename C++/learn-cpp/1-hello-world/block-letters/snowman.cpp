8088a3fd2a473cb7e9a02c38aa48dfdf6f29a598
Pxngs <kaleb_thomas2@icloud.com>
2026-01-09 09:21:23 -0500

    Create snowman.cpp

---
+++ C++/learn-cpp/1-hello-world/block-letters/snowman.cpp
@@ -0,0 +1,27 @@
+  #include <iostream>
+
+  int main () {
+
+    std::cout << "    *                    *        \n";                  
+    std::cout << "*        *                        \n";
+    std::cout << " *                *             * \n";
+    std::cout << "        *     *                   \n":
+    std::cout << "    *          *          *       \n";
+    std::cout << "             HHHHHHH              \n";
+    std::cout << "             HHHHHHH   *     *    \n";
+    std::cout << "           HHHHHHHHHHH            \n";
+    std::cout << "          *   @ @    *            \n";
+    std::cout << "         *     ^      *           \n";
+    std::cout << "          *         *             \n";
+    std::cout << "           SSS * * S              \n";
+    std::cout << "           * SSSSSSSS *           \n";
+    std::cout << "         *      0       *         \n":
+    std::cout << "        *        0      *         \n";
+    std::cout << "        *        0      *         \n";
+    std::cout << "         *             *          \n";  
+    std::cout << "            *   *   *             \n";
+    std::cout << "                                  \n";
+
+    return 0;
+
+  }
