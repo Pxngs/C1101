f5f77699a1d475dcc55e898ad124fdf155e92bfa
Pxngs <kaleb_thomas2@icloud.com>
2026-01-15 08:27:39 -0500

    Update snoop_dog.cpp

--- C++/learn-cpp/2-variables/dog-years/snoop_dog.cpp
+++ C++/learn-cpp/2-variables/dog-years/snoop_dog.cpp
@@ -1,1 +1,24 @@
 // do this later. huge ahh learning jump
+#include <iostream>
+
+int main () {
+
+  // Allow user to enter dog's name
+  std::string dog_name;
+  std::cout << "Enter dog's name: ";
+  std::cin >> dog_name;
+
+  // Allow user to enter dogs age
+  double dog_age;
+  std::cout << "Enter dog's age: ";
+  std::cin >> dog_age;
+
+  double early_years, later_years, later_years;
+
+  // First two years
+  early_years = 21;
+
+  // Accounts for later years
+  later_years = (dog_age - 2 ) * 4;
+
+  // My logic on puppy math
