7934482a5ae06c61ba509538fd764db8775799b1
Pxngs <kaleb_thomas2@icloud.com>
2026-01-22 11:15:59 -0500

    Create space.cpp

---
+++ C++/learn-cpp/3-conditionals-and-logic/space.cpp
@@ -0,0 +1,13 @@
+#include <iostream> 
+
+int main () {
+
+  double weight;
+  int x;
+
+  std::cout << "Please enter your current earth weight: ";
+  std::cin >> weight;
+
+  std::cout << "";
+  std::cout << "";  
+  std::cout << "":
