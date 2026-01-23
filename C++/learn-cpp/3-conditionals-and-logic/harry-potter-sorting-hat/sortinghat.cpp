505a2e13ef6083a2bd24a136ae146d9b04aa5a8b
Pxngs <kaleb_thomas2@icloud.com>
2026-01-23 09:11:57 -0500

    Create sortinghat.cpp

---
+++ C++/learn-cpp/3-conditionals-and-logic/harry-potter-sorting-hat/sortinghat.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main () {
+
+  int gryffindor = 0;
+  int hufflepuff = 0;
+  int ravenclaw = 0;
+  int slytherin = 0;
+
+  int answer1, answer2, answer3, answer4;
+
+  std::cout << "===============\n";
+  std::cout << "The Sorting Hat\n";
+  std::cout << "===============\n";
+
+  // ~~~~~~~~~~ Question 1 ~~~~~~~~~~
+
+  std::cout << "nQ1) ";
