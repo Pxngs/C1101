872844a3d48772c7305417d0061264c2c17a50f5
Pxngs <kaleb_thomas2@icloud.com>
2026-02-06 09:16:16 -0500

    Update magic8.cpp

--- C++/learn-cpp/3-conditionals-and-logic/magic-8-ball/magic8.cpp
+++ C++/learn-cpp/3-conditionals-and-logic/magic-8-ball/magic8.cpp
@@ -3,4 +3,37 @@
 
 int main () {
 
-  std::cout << "MAGIC 🎱 SAYS
+  std::cout << "MAGIC 🎱 SAYS: \n\n";
+
+  srand(time(NULL));
+
+  int choice = std::rand() % 20;
+
+  if (choice == 0) {
+
+    std::cout << "It is certain.\n";
+
+  }
+  else if (choice == 1) {
+
+    std::cout << "It is decidedly so.\n";
+
+  }
+  else if (choice == 2) {
+
+    std::cout << "Without a doubt.\n";
+
+  }
+  else if (choice == 3) {
+
+    std::cout << "Yes - definently.\n";
+
+  }
+  else if (choice == 4) {
+
+    std::cout << "You may rely on it.\n";
+
+  }
+  else if (choice == 5) {
+
+    std::cout << "
