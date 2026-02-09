61d22fa1d74e1b114d866c9cd7e18a4308d5956a
Pxngs <kaleb_thomas2@icloud.com>
2026-02-09 14:13:19 -0500

    Create RPSLS_switch_scores.cpp

---
+++ C++/learn-cpp/3-conditionals-and-logic/rock-paper-scissors-lizard-spock/RPSLS_switch_scores.cpp
@@ -0,0 +1,22 @@
+/* GAME LOGIC:
+
+Scissors cut Paper
+Paper covers Rock
+Rock crushes Lizard
+Lizard poisons Spock
+Spock smashes Scissors
+Scissors decapitate Lizard
+Lizard eats Paper
+Paper disproves Spock
+Spock vaporizes Rock
+Rock crushes Scissors
+
+*/
+
+#include <iostream>
+#include <stdlib.h>
+
+// Defining structs for returning results
+struct Results {
+  int user_score;
+  int computer_score;
