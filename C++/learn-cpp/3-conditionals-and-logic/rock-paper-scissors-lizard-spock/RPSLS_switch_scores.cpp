f041def7704ba643e3f9e234378a0456b263583c
Pxngs <kaleb_thomas2@icloud.com>
2026-02-18 09:18:27 -0500

    Update RPSLS_switch_scores.cpp

--- C++/learn-cpp/3-conditionals-and-logic/rock-paper-scissors-lizard-spock/RPSLS_switch_scores.cpp
+++ C++/learn-cpp/3-conditionals-and-logic/rock-paper-scissors-lizard-spock/RPSLS_switch_scores.cpp
@@ -53,8 +53,41 @@ void AnnounceResults(int user, int computer, std::string result) {
 }
 
 Results PlayWithComputer(int user, int computer, int user_score, int computer_score) {
-  if (user == computer) }
+  if (user == computer) {
     return {user_score, computer_score, "It is a draw!"};
+  }
 
+  switch (user) {
+    case 1: // Rock
+      if (computer == 3 || computer == 4) // Scissors & Lizard
+      {
+        user_score++
+        Return {user_score, computer_score, "You Win!";
+      }
+      else
+      {
+        computer_score++;
+        return {user_score, computer_score, "You Lose!";
+      }
+      break;
 
+    case 2: // Paper
+      if (computer == 1 || computer == 5) // Rock & Spock
+      {
+        user_score++;
+        return {user_score, computer_score, "You Win!";
+      }
+      break;
 
+    case 3: // Scissors
+      if (computer == 2 || computer == 4) // Paper & Lizard
+      }
+        user_score++
+        return {user_score, computer_score, "You Win!"};
+      }
+      else
+      {
+        computer_score++;
+        return {user_score, computer_score, "You Lose!"};
+      }
+      break;
