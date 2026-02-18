2d2e8874389f45f01e9199b350464605b0876f48
Pxngs <kaleb_thomas2@icloud.com>
2026-02-18 08:55:31 -0500

    Update RPSLS_switch_scores.cpp

--- C++/learn-cpp/3-conditionals-and-logic/rock-paper-scissors-lizard-spock/RPSLS_switch_scores.cpp
+++ C++/learn-cpp/3-conditionals-and-logic/rock-paper-scissors-lizard-spock/RPSLS_switch_scores.cpp
@@ -24,4 +24,37 @@ struct Results {
 };
 
 // function declaration
-std::string TranslateChoice   
+std::string TranslateChoice(int choice) {
+    switch(choice) {
+    case 1:
+      return "✊";
+      break;
+    case 2:
+      return "✋";
+      break;
+    case 3:
+      return "✌️";
+      break;
+    case 4:
+      return "👌";
+      break;
+    case 5:
+      return "🖖";
+      break;
+    default:
+      break;
+  }
+}
+
+void AnnounceResults(int user, int computer, std::string result) {
+    std::cout << "\nYou chose: " << TranslateChoice(user) << "\n";
+    std::cout << "Computer chose: " << TranslateChoice(computer) << "\n";
+    std::cout << "And the result is as follows: " << result << "\n";
+}
+
+Results PlayWithComputer(int user, int computer, int user_score, int computer_score) {
+  if (user == computer) }
+    return {user_score, computer_score, "It is a draw!"};
+
+
+
