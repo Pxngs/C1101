e0c0aa55af25a97a28d1b76a6b319bc9595bf2c1
kau.roi <kaleb_thomas2@icloud.com>
2026-06-03 08:55:56 -0400

    Update RPSLS.cpp

--- C++/learn-cpp/3-conditionals-and-logic/rock-paper-scissors-lizard-spock/RPSLS.cpp
+++ C++/learn-cpp/3-conditionals-and-logic/rock-paper-scissors-lizard-spock/RPSLS.cpp
@@ -8,4 +8,48 @@ This program will allow you to play RPSLS against the computer
 int main() {
 
   srand(time(NULL));
-  int 
+  int computer == std::rand() % 5 + 1;
+  int user = 0;
+
+  std::cout << "=================================\n";
+  std::cout << "rock paper scissors lizards spock!\n";
+  std::cout << "=================================\n";
+
+  std::cout << "1) ✊\n";
+  std::cout << "2) ✋\n";
+  std::cout << "3) ✌️\n";
+  std::cout << "4) lizard\n";
+  std::cout << "5) spock\n\n";
+
+  std::cout << "shoot:  ";
+
+  int rock = 1;
+  int paper = 2;
+  int scissors = 3;
+  int lizard = 4;
+  int spock = 5;
+
+  std::cin >> user;
+
+  /*
+  rock > scissors
+  scissors > paper
+  paper > rock
+  lizard > spock
+  spock > scissors
+  scissors > lizard
+  lizard > paper
+  paper > spock
+  spock > rock
+  rock > lizard
+  */
+
+  std::cout << "computer: " << computer << "\n\n";
+
+  if (user == rock && computer == scissors) {
+
+    std::cout << "You Win!\n";
+
+  } else if (user == scissors && computer = 
+
+
